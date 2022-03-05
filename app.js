ea6cf03346194267337e67d424b9260cc346e8a2
Ahmad Hannan <54240600+ahmadhah@users.noreply.github.com>
2022-03-05 13:17:55 +0500

    k

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
         app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`)
+            console.log(`Example app listening o port ${port}`)
         })
     }
     catch (err) {
